43119ed13e28a168318c9d99ec8f176ef01368d4
Ann Kim <annkim822@gmail.com>
2018-06-20 22:18:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,14 @@ def roll_call_dwarves(names)
   end 
 end
 
-def summon_captain_planet
+def summon_captain_planet(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+    new_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
